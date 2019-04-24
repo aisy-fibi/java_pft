467aa64fac2b82dbbd80bbd127e2ce47ddd8d734
Yulia Brus <brus.iulia@gmail.com>
2019-04-24 23:03:10 +0200

    User Creation Test refactoring

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTest.java
@@ -14,35 +14,55 @@ public class UserCreationTest {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
   }
 
   @Test
   public void testUserCreation() throws Exception {
 
-    wd.findElement(By.linkText("add new")).click();
+    gotoAddNewUserPage();
+    fillUserData(new UserData("Qwertyu", "Asdfghj", "Test", "Main strett, 25", "test@test.com"));
+    submitUserCreation();
+    logout();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitUserCreation() {
+    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
+  }
+
+  private void fillUserData(UserData userData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Qwertyu");
+    wd.findElement(By.name("firstname")).sendKeys(userData.getFirstname());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Asdfghj");
+    wd.findElement(By.name("lastname")).sendKeys(userData.getLastname());
     wd.findElement(By.name("nickname")).click();
     wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("Test");
+    wd.findElement(By.name("nickname")).sendKeys(userData.getNickname());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("Main strett, 25");
+    wd.findElement(By.name("address")).sendKeys(userData.getAddress());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("test@test.com");
-    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
-    wd.findElement(By.linkText("Logout")).click();
+    wd.findElement(By.name("email")).sendKeys(userData.getEmail());
+  }
+
+  private void gotoAddNewUserPage() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod(alwaysRun = true)
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class UserData {
+  private final String firstname;
+  private final String lastname;
+  private final String nickname;
+  private final String address;
+  private final String email;
+
+  public UserData(String firstname, String lastname, String nickname, String address, String email) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.address = address;
+    this.email = email;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
