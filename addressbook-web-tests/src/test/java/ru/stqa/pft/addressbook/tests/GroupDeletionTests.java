4400160f03d370cde00949c1a5385b7925498e53
Yulia Brus <brus.iulia@gmail.com>
2019-05-10 17:46:28 +0200

    Comparing of the collections, before and after removing group

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -20,6 +20,9 @@ public class GroupDeletionTests extends TestBase {
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
-  }
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before,after);//тут можно убрать цикл, потому что тестовый фрейсворк умеет сам сранивать списки поэлементно, без цикла
+   }
 
 }
