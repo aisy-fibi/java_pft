837ceed50fe206622c5d6f1e7d5d3b65f0b7de25
Yulia Brus <brus.iulia@gmail.com>
2019-05-11 23:30:20 +0200

    Group creation Tests, updating with max ID

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -13,12 +14,21 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();//будет содержать список элементов до того, как создана новая группа
-    for (int i = 0; i < before.size(); i++){
-      System.out.println("Before " + i + before.get(i));
-    }
-    app.getGroupHelper().createGroup(new GroupData("Test 1", null, null));
+    GroupData group = new GroupData("Test 1", null, null);
+    app.getGroupHelper().createGroup(group);
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList(); // будет содержать список элементов после того, как создана новая группа
     Assert.assertEquals(after.size(), before.size() + 1);//размер списка до создания новой группы должен быть на один меньше, чем после создания
+
+
+    int max = 0;
+    for (GroupData g : after){
+      if (g.getId() > max){
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
