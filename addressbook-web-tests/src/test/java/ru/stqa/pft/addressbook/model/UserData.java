67652a8c4c1d0199cd63a665387be208304922b3
Yulia Brus <brus.iulia@gmail.com>
2019-05-12 22:52:59 +0200

    Sorting of the lists

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/UserData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/UserData.java
@@ -9,8 +9,27 @@ public class UserData {
   private final String email;
   private final String group;
 
-  public UserData( String firstname, String lastname, String nickname, String address, String email, String group) {
-    this.id = 0;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    UserData userData = (UserData) o;
+
+    if (firstname != null ? !firstname.equals(userData.firstname) : userData.firstname != null) return false;
+    return lastname != null ? lastname.equals(userData.lastname) : userData.lastname == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstname != null ? firstname.hashCode() : 0;
+    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    return result;
+  }
+
+  public UserData(String firstname, String lastname, String nickname, String address, String email, String group) {
+    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.lastname = lastname;
     this.nickname = nickname;
@@ -32,28 +51,6 @@ public class UserData {
   public void setId(int id) {
     this.id = id;
   }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    UserData userData = (UserData) o;
-
-    if (id != userData.id) return false;
-    if (firstname != null ? !firstname.equals(userData.firstname) : userData.firstname != null) return false;
-    return lastname != null ? lastname.equals(userData.lastname) : userData.lastname == null;
-
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
-    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-    return result;
-  }
-
 
 
   @Override
